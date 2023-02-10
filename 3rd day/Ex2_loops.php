b875b4245d4121115dc6cf7444dbc001b797971f
ThaiHoang-newbie <thaihoang20112k3@gmail.com>
2023-02-10 08:56:24 +0700

    Update input form instead random variable

--- 3rd day/Ex2_loops.php
+++ 3rd day/Ex2_loops.php
@@ -1,23 +1,33 @@
+<form method="POST" action="">
+    Num1:
+    <input type="text" name="d">
+    Num2:
+    <input type="text" name="r">
+
+    <input type="submit" name="btn1">
+</form>
+
 <?php #Ve hinh chu nhat bang cac dau * khi cho chieu dai va rong truoc
+if (isset($_POST["btn1"])) {
+    $d = $_POST['d'];
+    $r = $_POST['r'];
 
-$d = rand(3,10);
-$r = rand(3, 10);
-$sum = "";
+    for ($i = 0; $i < $r; $i++){
+        for ($j = 0; $j < $d; $j++){
+            echo "*";
+        }
+        echo "<br>";
+        ;
+    };
 
-
-for ($i = 0; $i < $r; $i++){
-    for ($j = 0; $j < $d; $j++){
-        echo "*";
+    if ($r == $d){
+        echo "Đây là hình vuông với cạnh là $r";
+    }else if($r > $d){
+        echo "Đây là hình chữ nhật với chiều dài là $r và chiều rộng là $d";
+    }else{
+        echo "Đây là hình chữ nhật với chiều dài là $d và chiều rộng là $r";
     }
-    echo "<br>";
-    ;
-};
-if ($r == $d){
-    echo "Đây là hình vuông với cạnh là $r";
-}else if($r > $d){
-    echo "Đây là hình chữ nhật với chiều dài là $r và chiều rộng là $d";
-}else{
-    echo "Đây là hình chữ nhật với chiều dài là $d và chiều rộng là $r";
 }
+;
 
 ?>
