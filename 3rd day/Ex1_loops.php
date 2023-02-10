2cc7cf8c38cc165858f018de9cd825b0cd573188
ThaiHoang-newbie <thaihoang20112k3@gmail.com>
2023-02-10 07:59:13 +0700

    New update ex3

--- 3rd day/Ex1_loops.php
+++ 3rd day/Ex1_loops.php
@@ -1,13 +1,7 @@
-<table border="1px">
-<tr>
 <?php # Bang cuu chuong
 
 $n = 7;
-    for($j = 1; $j <= $n; $j ++) {
-        echo "$n x $n = " . ($j * $n);
-        echo "<br>";
-    }
-
-?>
-</tr>
-</table>
+for ($j = 1; $j <= $n; $j++) {
+    echo "$n x $n = " . ($j * $n);
+    echo "<br>";
+}
