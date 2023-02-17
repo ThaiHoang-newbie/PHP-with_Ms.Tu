d263fdcc586a1c9aabf19a9270d36dfdfa4f3962
ThaiHoang-newbie <thaihoang20112k3@gmail.com>
2023-02-17 08:06:14 +0700

    New update

--- 5th day/Ex1_form.php
+++ 5th day/Ex1_form.php
@@ -2,29 +2,43 @@
 # hãy in ra màn hình số lớn nhất và nhỏ nhất của mảng. Cho biết vị trí của các phần tử đó
 
 // Max
-function getMax($array)
+function getMax($array, $all)
 {
-   $n = count($array);
-   $max = $array[0];
-   for ($i = 1; $i < $n; $i++)
-       if ($max < $array[$i])
-           $max = $array[$i];
-    return $max;      
+    $n = count($array);
+    $max = $array[0];
+    for ($i = 1; $i < $n; $i++)
+        if ($max < $array[$i]) {
+            $max = $array[$i];
+        }
+    for ($i = 0; $i < $n; $i++)
+        if ($max == $array[$i]) {
+            $all = $all . ($i+1). " ";
+        }
+    return "Số lớn nhất là: " . $max . " ở vị trí " . $all.".";
 }
+
 // Min
-function getMin($array)
+function getMin($array, $all)
 {
-   $n = count($array);
-   $min = $array[0];
-   for ($i = 1; $i < $n; $i++)
-       if ($min > $array[$i])
-           $min = $array[$i];
-    return $min;      
+    $n = count($array);
+    $min = $array[0];
+    for ($i = 1; $i < $n; $i++)
+        if ($min > $array[$i]) {
+            $min = $array[$i];
+        }
+    for ($i = 0; $i < $n; $i++)
+        if ($min == $array[$i]) {
+            $all = $all . ($i+1). " ";
+        }
+    return "Số nhỏ nhất là: " . $min . " ở vị trí " . $all;
 }
 
-$array = array(1, 2, 3, 4, 5);
+$all = "";
+$array = array(1, 2, 3, 4, 5, 5, 5);
 
-echo(getMax($array));
-echo("\n");
-echo(getMin($array));
+echo (getMax($array, $all));
+echo ("\n");
+echo ("\n");
+
+echo (getMin($array, $all));
 ?>
