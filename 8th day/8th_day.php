e1ea89a34b0a761da133136bee6ff89c11746d96
ThaiHoang-newbie <thaihoang20112k3@gmail.com>
2023-02-27 07:55:48 +0700

    New ex

--- 8th day/8th_day.php
+++ 8th day/8th_day.php
@@ -1,20 +1,41 @@
-<form action="" method="Post" enctype="Multipart/from-data">
-    <input type="file" name="file-up" accept="">
-    <input type="submit" name="btn" value="upload_file">
-</form>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<style>
+</style>
+
+<body>
+    <form action="#" method="post" enctype="multipart/form-data">
+        <input type="file" name="hinhanh" value="upload">
+        <input type="submit" name="btn" value="Gửi">
+    </form>
+</body>
+
+</html>
 <?php
+
 if (isset($_POST['btn'])) {
-    if (isset($_FILES['upload_file'])) {
-        $file = $_FILES['upload_file'];
-    
-
-    $namefile = $file['file-up'];
-    move_uploaded_file($file['tmp_name'], $namefile);
+    if (isset($_FILES['hinhanh'])) {
+        if ($_FILES['hinhanh']['size'] == 0) {
+            echo "Bạn chưa chọn hình ảnh!";
+        } else {
+            if (isset($_FILES['hinhanh'])) {
+                $file = $_FILES['hinhanh'];
+                $tenfile = $file['name'];
+                move_uploaded_file($file['tmp_name'], $tenfile);
 ?>
-    <img src="<?php echo $namefile ?>" height="100" width="70" />
+                <img src='<?php echo $tenfile; ?>' width="100" ; height="100">
 <?php
+                echo "<br>";
+                echo $file['name'];
+            }
+        }
+    }
 }
-}
-
-
 ?>
---
+++ 8th day/asd.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <form action="#" method="post" enctype="multipart/form-data">
+        <input type="file" name="hinhanh" value="upload">
+        <input type="submit" name="btn" value="Gửi">
+    </form>
+</body>
+
+</html>
+<?php
+
+if (isset($_POST['btn'])) {
+    if (isset($_FILES['hinhanh'])) {
+        if ($_FILES['hinhanh']['size'] == 0) {
+            echo "Bạn chưa chọn hình ảnh!";
+        } else {
+            if (isset($_FILES['hinhanh'])) {
+                $file = $_FILES['hinhanh'];
+                $tenfile = $file['name'];
+                move_uploaded_file($file['tmp_name'],$tenfile);
+                echo "Upload hình ảnh thành công!";
+?>
+                <img src='<?php echo $tenfile ?>' width="100" ; height="100">
+<?php
+            }
+        }
+    }
+}
+
+
+
+?>
